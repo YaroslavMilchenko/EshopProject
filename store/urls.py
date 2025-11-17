525e90e97f92e56325fc1fed13f7a495d509da72
Yaroslav Milchenko <zadirakalena@gmail.com>
2025-11-17 14:26:40 +0200

    cart and add to cart + work with sessin

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ from .views import SignUpView
 urlpatterns = [
     path('', views.category_list, name = 'category-list'),
     path('category/<int:category_id>/', views.product_list, name = 'product-list'),
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.cart_detail, name='cart-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Category, Product
 from django.views import generic
 from .forms import CustomerUserCreationForm
@@ -27,4 +28,44 @@ class SignUpView(generic.CreateView):
     form_class = CustomerUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+def add_to_cart(request, product_id):
+    if request.method == 'POST':
+        cart = request.session.get('cart', {}) #get session cart
+        product_id_str = str(product_id)
+        quantity = cart.get(product_id_str, 0)  #get current quantity of the product
+        cart[product_id_str] = quantity + 1
+        request.session['cart'] = cart      #save cart back
+        request.session.modified = True     #(Important) Tell Django that the session has been modified
+        messages.success(request, f'The product has been successfully added to your cart.')
+        next_page = request.POST.get('next', 'category-list')
+    return redirect(next_page)      
+    # (request.META.get('HTTP_REFERER') is the "previous URL")
+
+def cart_detail(request):
+    cart = request.session.get('cart', {})  #get cart from session or empty dictionary
+    product_ids = cart.keys()
+    products = Product.objects.filter(id__in=product_ids)   #get objects from BD by product_ids
+    cart_items = []
+    total_price = 0
     
+    for product in products:
+        quantity = cart[str(product.id)]    #get quantity from session by id in dictionary
+        item_total_price = product.price * quantity   #total price for this product
+        
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'item_total_price': item_total_price,
+        })
+        
+        total_price += item_total_price
+    
+    context = {
+        'cart_items': cart_items,
+        'total_price': total_price,
+    }
+    
+    return render(request, 'store/cart_detail.html', context)
+        
+        
